78e993b3a19839d0b1aeef3a735a7d3ef94b0b45
Durgesh Kudalkar <64612410+durgesh-k@users.noreply.github.com>
2025-01-26 21:54:40 +0530

    Changed file picking method from path to bytes for web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1719755442"
+    serviceWorkerVersion: "3728475289"
   }
 });
